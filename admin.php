292b24b38acdfad406b932aff46f92a096bd4ff1
DavidManiIbrahim <ibrahimmanidavid@gmail.com>
2024-11-09 02:52:17 +0100

    added sql

---
+++ admin.php
@@ -0,0 +1,32 @@
+<?php 
+
+include("config.php");
+
+$query = "SELECT * FROM suggestions";
+$result = mysqli_query($conn, $query);
+
+echo"<table><tr>";
+while($row = mysqli_fetch_array($result)){ 
+    ?>
+ <td><?php echo $row['suggestion']."<br>"?></td>
+ 
+<?php 
+
+}
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
